bc7eaf05dd23b6b3601301fbc3491b31a67f6648
Erwin Quek <72030222+erwinqxy@users.noreply.github.com>
2022-07-18 03:44:16 +0800

    Reorder buttons

--- components/Links.jsx
+++ components/Links.jsx
@@ -6,11 +6,11 @@ export default function Links() {
       <a href="https://docs.google.com/document/d/1rQiFf5l_LW3wo7sDMSsfy9mGsUn1JN4a/edit?usp=sharing&ouid=111099942703627877902&rtpof=true&sd=true" target="_blank">
         <button> Resume </button>
       </a>
-      <a href="https://github.com/erwinqxy" target="_blank">
-        <button> Github </button>
-      </a>
       <a href="https://erwinquek.vercel.app/" target="_blank">
         <button> Personal Site </button>
+      </a>
+      <a href="https://github.com/erwinqxy" target="_blank">
+        <button> Github </button>
       </a>
     </div>
   );
