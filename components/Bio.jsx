fad5685c06350104b55d24b8252b03d9b18ac7dd
Erwin Quek <72030222+erwinqxy@users.noreply.github.com>
2022-07-18 03:35:45 +0800

    Fix picture bug:

--- components/Bio.jsx
+++ components/Bio.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import ProfilePic from '../src/assets/profile.jpeg'
 
 export default function Bio() {
     return (
         <div className="Bio">
-            <img src="[../src/assets/profile.jpeg]" className="profile-pic" />
+            <img src={ProfilePic} className="profile-pic" />
             <h1>Erwin Quek</h1>
             <h2>Student Developer</h2>
             <h3>erwin.quek@u.nus.edu</h3>
